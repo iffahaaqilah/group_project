e51314645d87eeb64b3cf791ecf92cc6bf81c9ea
BatrisyiaSR <dknbat@gmail.com>
2023-08-25 10:29:38 +0800

    Batrisyia - 20B2110

---
+++ dicts.py
@@ -0,0 +1,15 @@
+def Merge(dict_1, dict_2):
+    result = dict_1 | dict_2
+    return result
+
+dict_1 = dict(input("Enter key and value: ").split() for i in range(3))
+dict_2 = dict(input("Enter key and value: ").split() for j in range(3))
+dict_3 = Merge(dict_1, dict_2)
+print(dict_3)
+
+
+
+
+
+
+	
