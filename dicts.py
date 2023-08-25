0a9f062b30efe3e9574aa465501bdc494fed89bb
19B2097 <yasya7121@gmail.com>
2023-08-25 11:21:22 +0800

    Batrisyia 20B2110 Syasya 19B2097

--- dicts.py
+++ dicts.py
@@ -6,7 +6,7 @@ dict_1 = dict(input("Enter key and value: ").split() for i in range(3))
 dict_2 = dict(input("Enter key and value: ").split() for j in range(3))
 dict_3 = Merge(dict_1, dict_2)
 print(dict_3)
-
+#hello world
 
 
 
